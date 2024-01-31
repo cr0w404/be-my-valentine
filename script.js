755b7cb93e408362306a4ceb175e17e21efc68cc
cr0w404 <140859338+cr0w404@users.noreply.github.com>
2024-01-31 22:35:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "Every time you click on NO, you will get one less kiss";
+      "Every time you click on NO, you will get one kiss less";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
