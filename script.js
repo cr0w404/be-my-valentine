87eb28e0ecddfa13b6dbd1703a123e74f8e3c8b8
cr0w404 <140859338+cr0w404@users.noreply.github.com>
2024-01-31 22:10:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showMessage(response) {
     noButton.style.position = "absolute";
 
     // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+    document.getElementsByClassName("image")[0].src = "images/dance-techno.gif";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
